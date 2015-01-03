d62d48b4ffd0ad23acf93280cb85a32089cf349b
Marco Giordano <marco.giordano.work@gmail.com>
2015-01-03 10:46:59 +0000

    cleaned up bw_kernel passing at blur

--- src/bw_kernel.cu
+++ src/bw_kernel.cu
@@ -1,15 +1,10 @@
 
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
-#include <cuda.h>
 
-
-#include<stdlib.h>
 #include <stdint.h>
 #include<iostream>
 #include<stdio.h>
-
-//cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
 
 __global__ void bw_kernel(const uint8_t *d_source, uint8_t *d_target, 
 						const int width, const int height)
@@ -38,34 +33,33 @@ __global__ void bw_kernel(const uint8_t *d_source, uint8_t *d_target,
 void run_bw_kernel(const uint8_t *d_source,  uint8_t *d_target, 
 						const int width, const int height)
 {
-	int grainSize=16;
+	const int grainSize=16;
 	int width_blocks,width_height;
 	//computing the block size
 	width_blocks = ((width%grainSize) != 0)?(width/grainSize) +1: (width/grainSize);
 	width_height = ((height%grainSize) != 0)?(height/grainSize) +1: (height/grainSize);
-
-	std::cout<<width_blocks<<std::endl;
-	std::cout<<width_height<<std::endl;
-
-	std::cout<<((width_blocks*width_height*grainSize*grainSize)-(width*height))<<std::endl;
+	
+	//setupping the block and grids
 	const dim3 blockSize( grainSize, grainSize , 1); 
 	const dim3 gridSize( width_blocks, width_height, 1); 
 
 	//setupping clock
-
 	cudaEvent_t start, stop;
 	cudaEventCreate(&start);
 	cudaEventCreate(&stop);
 
+	//starting the clock
 	cudaEventRecord(start);
+	//kick the kernel
 	bw_kernel<<<gridSize, blockSize>>>(d_source, d_target, width,height);
+	//stop the clock
 	cudaEventRecord(stop);
 
 	//getting the time
 	float milliseconds = 0;
 	cudaEventElapsedTime(&milliseconds, start, stop);
+	//sincronizing device
 	cudaDeviceSynchronize();
-	//std::cout<<"kernel execution time in ms: "<<(milliseconds)<<std::endl;	
 	
 	//checking for error
 	cudaError_t err = cudaGetLastError();
