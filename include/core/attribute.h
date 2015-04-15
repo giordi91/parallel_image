b1fdadfd57a994004213bc88e806d1e37bae1ea9
Marco Giordano <marco.giordano.work@gmail.com>
2015-04-15 22:07:37 +0100

    first version of attribute now I will try to substitute that in the filters

--- include/core/attribute.h
+++ include/core/attribute.h
@@ -16,14 +16,20 @@ template<typename T>
 class AttributeTyped {
 	public:
 
-		
+		AttributeTyped(const char *name):m_name(name){};
+		AttributeTyped(const char *name ,T value):m_name(name),m_value(value){};
+
 		std::string type()
 		{
 			return typeid(T).name();
 		}
 
+		T get_value() const {return m_value;};
+		void set_value(T value){ m_value=value;};
+		std::string get_name(){return m_name;};
 	private:
 
+		std::string m_name;
 		T m_value;
 };
 
--- tests/attribute_tests.cpp
+++ tests/attribute_tests.cpp
@@ -1,7 +1,5 @@
-#include <cstdint> // uint8_t declaration
 #include <gmock/gmock.h>
 #include <iostream>
-#include <stdexcept>      // std::invalid_argument
 #include <core/attribute.h>
 using namespace testing;
 
@@ -9,10 +7,59 @@ using namespace testing;
 
 TEST(Attribute_test, basic_instances)
 {
-	AttributeTyped<int> intAttr;
-	AttributeTyped<float> floatAttr;
+	AttributeTyped<int> intAttr("intTest");
+	AttributeTyped<float> floatAttr("floatTest");
+	AttributeTyped<bool> boolAttr("boolTest");
+}
 
-	std::cout<<intAttr.type()<<std::endl;
-	std::cout<<floatAttr.type()<<std::endl;
+TEST(Attribute_test, default_value)
+{
+	AttributeTyped<int> intAttr("intTest",11);
+	AttributeTyped<float> floatAttr("floatTest",1.2f);
+	AttributeTyped<bool> boolAttr("boolTest",true);
+
+	EXPECT_EQ(intAttr.get_value(),11);
+	EXPECT_EQ(floatAttr.get_value(),1.2f);
+	EXPECT_EQ(boolAttr.get_value(),true);
 
 }
+
+TEST(Attribute_test, set_get_value)
+{
+	AttributeTyped<int> intAttr("intTest");
+	AttributeTyped<float> floatAttr("floatTest");
+	AttributeTyped<bool> boolAttr("boolTest");
+
+	intAttr.set_value(10);	
+	floatAttr.set_value(5.0f);
+	boolAttr.set_value(false);	
+
+	EXPECT_EQ(intAttr.get_value(),10);
+	EXPECT_EQ(floatAttr.get_value(),5.0f);
+	EXPECT_EQ(boolAttr.get_value(),false);
+
+
+	intAttr.set_value(-1);	
+	floatAttr.set_value(15.2f);
+	boolAttr.set_value(true);	
+
+	EXPECT_EQ(intAttr.get_value(),-1);
+	EXPECT_EQ(floatAttr.get_value(),15.2f);
+	EXPECT_EQ(boolAttr.get_value(),true);
+}
+
+TEST(Attribute_test, ensure_name)
+{
+	AttributeTyped<int> intAttr("intTest");
+	AttributeTyped<float> floatAttr("floatTest");
+	AttributeTyped<bool> boolAttr("boolTest");
+
+	EXPECT_EQ(intAttr.get_name(),"intTest");
+	EXPECT_EQ(floatAttr.get_name(),"floatTest");
+	EXPECT_EQ(boolAttr.get_name(),"boolTest");
+}
+
+//specs
+//template attribute
+//get set value
+//get_set name
